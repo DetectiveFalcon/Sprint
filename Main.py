b139d5c32b573010fb5174480067839d7d26de3b
Abdiel Falcon <61768345+DetectiveFalcon@users.noreply.github.com>
2020-10-26 19:12:42 -0400

    Simple Comment
    I just added a simple comment.

--- Main.py
+++ Main.py
@@ -182,4 +182,5 @@ def btn(userName):
     print("Congratulations " + userName + " you have mastered this subject\nReturning to Suject Selection...")
     subjectSelection(userName)
 #This starts the program calling the main function#
+#Your code should be shorter, JK. I Love YOU #
 main()
